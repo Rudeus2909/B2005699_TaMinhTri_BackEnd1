2b9e2820d403d74587b6ab0894fc7a36ae529187
Rudeus2909 <trib2005699@student.ctu.edu.vn>
2024-01-06 15:40:35 +0700

    Cài đặt xử lý lỗi

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ const cors = require("cors");
 
 const contactsRouter = require("./app/routes/contact.route");
 
+const ApiError = require("./app/api-error");
+
 const app = express();
 
-app.unsubscribe(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -13,5 +14,15 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactsRouter);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 
 module.exports = app;
